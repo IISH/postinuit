c85eeb71ba8c28b522f9bfa72800406896a96ba8
Gordan Cupac <gordan-cupac@users.noreply.github.com>
2017-08-09 17:33:23 +0200

    small changes

--- retrieve_data_autocomplete.php
+++ retrieve_data_autocomplete.php
@@ -4,20 +4,28 @@ require_once "classes/start.inc.php";
 // check if an user is logged in
 $oWebuser->checkLoggedIn();
 
-global $dbConn;
-
+//
+$minLengthSearchTerm = 2;
 $result = array();
-// TODO: security!!!!
-$searchterm =  addslashes(substr(trim($_GET['term']), 0,20));
-$table = addslashes(trim($_GET['table']));
-$column = addslashes(trim($_GET['column']));
-// TODO: controleer of searchterm minimaal 3 characters lang is.
-$query = "SELECT ".$column." FROM ".$table." WHERE ".$column." LIKE '%".$searchterm."%' GROUP BY ".$column." LIMIT 0,10";
-$stmt = $dbConn->getConnection()->prepare($query);
-$stmt->execute();
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $result[] = $row[$column];
+// TODO: security!!!!
+// table en column moeten niet zichtbaar zijn/opgegeven worden in url, maar een cijfer (of code)
+$searchterm =  substr(trim($_GET['term']), 0,20);
+$table = trim($_GET['table']);
+$column = trim($_GET['column']);
+
+// conroleer of zoek term voldoet aan minimale lengte
+if ( strlen($searchterm) >= $minLengthSearchTerm ) {
+	//
+	$query = "SELECT " . addslashes($column) . " FROM " . addslashes($table) . " WHERE " . addslashes($column) . " LIKE '%" . addslashes($searchterm) . "%' GROUP BY " . addslashes($column) . " LIMIT 0,15 ";
+	$stmt = $dbConn->getConnection()->prepare($query);
+	$stmt->execute();
+
+	//
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$result[] = $row[$column];
+	}
 }
 
+//
 echo json_encode($result);
