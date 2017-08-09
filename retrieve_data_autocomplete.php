7ef16fa810fd571e4b55f65ab8f33b645ed1d2b2
Igor <igorvich1988@hotmail.com>
2017-08-09 16:14:38 +0200

    Small optimization of the query for autocomplete

--- retrieve_data_autocomplete.php
+++ retrieve_data_autocomplete.php
@@ -1,15 +1,23 @@
 <?php
 require_once "classes/start.inc.php";
 
+// check if an user is logged in
+$oWebuser->checkLoggedIn();
+
 global $dbConn;
 
-$searchterm = $_GET['term'];
-$table = $_GET['table'];
-$column = $_GET['column'];
-
-$query = "SELECT * FROM ".$table." WHERE ".$column." LIKE '".$searchterm."%'";
+$result = array();
+// TODO: security!!!!
+$searchterm =  addslashes(substr(trim($_GET['term']), 0,20));
+$table = addslashes(trim($_GET['table']));
+$column = addslashes(trim($_GET['column']));
+// TODO: controleer of searchterm minimaal 3 characters lang is.
+$query = "SELECT ".$column." FROM ".$table." WHERE ".$column." LIKE '%".$searchterm."%' GROUP BY ".$column." LIMIT 0,10";
 $stmt = $dbConn->getConnection()->prepare($query);
 $stmt->execute();
-$result = $stmt->fetchAll();
 
-echo json_encode($result[0]);
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $result[] = $row[$column];
+}
+
+echo json_encode($result);
