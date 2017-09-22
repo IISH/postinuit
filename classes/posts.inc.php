d151e7ce1db54eb7d50fea2fe2ecd9676426e66d
Gordan Cupac <gordan-cupac@users.noreply.github.com>
2017-09-22 14:54:12 +0200

    fixed bug download file with spaces in filename

--- classes/posts.inc.php
+++ classes/posts.inc.php
@@ -155,12 +155,13 @@ class Posts{
         if (file_exists($file)) {
             if (false !== ($handler = fopen($file, 'r'))) {
                 header('Content-Description: File Transfer');
-                header('Content-Disposition: attachment; filename=' . basename($file));
-                header('Content-Transfer-Encoding: binary');
+                header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+	            header('Content-Transfer-Encoding: binary');
+	            header('Content-Type: ' . mime_content_type($file));
+	            header('Content-Length: ' . filesize($file));
                 header('Expires: 0');
                 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
                 header('Pragma: public');
-                header('Content-Length: ' . filesize($file)); //Remove
 
                 readfile($file);
             }
