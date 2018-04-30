61e32e286cdc893064ba6e043f19a880b7d2c3af
Gordan Cupac <gordan-cupac@users.noreply.github.com>
2018-04-30 17:28:23 +0200

    wiki change

---
+++ htdocs/doresy/instructie_doresy.php
@@ -0,0 +1,14 @@
+<?php
+require_once "classes/start.inc.php";
+
+// check if an user is logged in
+$oWebuser->checkLoggedIn();
+
+$file = 'Instructie Doresy.pdf';
+
+header("Cache-Control: public");
+header("Content-Description: File Transfer");
+header("Content-Disposition: attachment; filename=\"$file\"");
+header("Content-Type: application/pdf");
+header("Content-Transfer-Encoding: binary");
+readfile(IniSettings::get('settings', 'wiki_directory') . $file);
